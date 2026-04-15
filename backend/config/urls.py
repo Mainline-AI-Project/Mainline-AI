4410e7e20d52161fb0b3e72f9228dbd155c72f6b
Madara Premawardhana <madarapremawardana@gmail.com>
2026-04-15 20:42:31 +0100

    deployment-frontendfix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path, re_path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("api/users/", include("apps.users.urls")),
     path("api/", include("api.urls")),
@@ -13,5 +16,10 @@ urlpatterns = [
     path("api/auth/reset-password/",  views.reset_password,  name="reset_password"),
     path('admin/', admin.site.urls),
 
-    re_path(r"^(?!api/).*", TemplateView.as_view(template_name="frontend/index.html")),
+ # Angular fallback LAST
+    re_path(r"^(?!api/).*",
+            TemplateView.as_view(template_name="frontend/index.html")),
 ]
+
+# ADD THIS
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
