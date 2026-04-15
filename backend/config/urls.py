464cf67eab9e017b7d70ef557f9dd3cd5f3826d3
Madara Premawardhana <madarapremawardana@gmail.com>
2026-04-15 18:59:14 +0100

    frontend-light

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,6 +3,8 @@ from apps.users.views import rag_query
 from apps.users import views
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.urls import re_path
 
 urlpatterns = [
     path("api/users/", include("apps.users.urls")),
@@ -11,5 +13,7 @@ urlpatterns = [
     path("api/auth/forgot-password/", views.forgot_password, name="forgot_password"),
     path("api/auth/reset-password/",  views.reset_password,  name="reset_password"),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='static/frontend/frontend/browser/index.html')),
+    path('', lambda request: HttpResponse("Backend is running ✅")),
+    re_path(r'^$', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),
 ]
--- frontend/dist/frontend/server/angular-app-manifest.mjs
+++ frontend/dist/frontend/server/angular-app-manifest.mjs
@@ -41,9 +41,9 @@ export default {
     'index.server.html': {size: 7325, hash: '0ee623197d89c5e9b25d9c1ade5d50ee4d27c0be5a07c913083d2ee33becd5f4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'reset-password/index.html': {size: 225, hash: 'f0d9e0108796e2e7b54a5ebcd679ace5ab8824b2f86a04284da20f61efcdadf1', text: () => import('./assets-chunks/reset-password_index_html.mjs').then(m => m.default)},
     'index.html': {size: 22884, hash: '9eeb272c79178a96fe0b683b9e7ce18a4cee19fb8f4f3c3dd52bb762c54d6fa3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'signup/index.html': {size: 16247, hash: '4ebc5bc491a47a0714da5057e0530b4e3426a6d3e60a13487d126fc6bc35ff98', text: () => import('./assets-chunks/signup_index_html.mjs').then(m => m.default)},
     'login/index.html': {size: 16247, hash: '4ebc5bc491a47a0714da5057e0530b4e3426a6d3e60a13487d126fc6bc35ff98', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
     'select-plan/index.html': {size: 16247, hash: '4ebc5bc491a47a0714da5057e0530b4e3426a6d3e60a13487d126fc6bc35ff98', text: () => import('./assets-chunks/select-plan_index_html.mjs').then(m => m.default)},
+    'signup/index.html': {size: 16247, hash: '4ebc5bc491a47a0714da5057e0530b4e3426a6d3e60a13487d126fc6bc35ff98', text: () => import('./assets-chunks/signup_index_html.mjs').then(m => m.default)},
     'styles-M3S3TEZ4.css': {size: 1298, hash: 'p7lU365+TkY', text: () => import('./assets-chunks/styles-M3S3TEZ4_css.mjs').then(m => m.default)}
   },
 };
