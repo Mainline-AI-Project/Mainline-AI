b7f9284fbdd38813cb834f7e09404e9671964445
Madara Premawardhana <madarapremawardana@gmail.com>
2026-04-15 19:56:12 +0100

    backend paths

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['/app/backend/frontend/browser']
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = '/app/backend/staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
