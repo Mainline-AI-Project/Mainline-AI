865629831c030468adff3f21297b9619b482e5ae
Madara Premawardhana <madarapremawardana@gmail.com>
2026-04-16 10:25:43 +0100

    Front-end fix

--- frontend/dist/frontend/server/angular-app-manifest.mjs
+++ frontend/dist/frontend/server/angular-app-manifest.mjs
@@ -2,48 +2,48 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/static/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/static"
   },
   {
     "renderMode": 2,
-    "route": "/login"
+    "route": "/static/login"
   },
   {
     "renderMode": 2,
-    "route": "/signup"
+    "route": "/static/signup"
   },
   {
     "renderMode": 2,
-    "route": "/select-plan"
+    "route": "/static/select-plan"
   },
   {
     "renderMode": 2,
-    "route": "/chat"
+    "route": "/static/chat"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/",
-    "route": "/**"
+    "redirectTo": "/static",
+    "route": "/static/**"
   },
   {
     "renderMode": 2,
-    "route": "/reset-password"
+    "route": "/static/reset-password"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 7723, hash: 'bfd5fbbe2acf79f9211dab4a2c6d7cd34d122af98ef39950c4d7d60f82ccf457', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7325, hash: 'bf9939d95047f83ccbe5112166cb2582e3a2905646c9077fdc44ab75488bc8c8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 16247, hash: '3318a62d72f25fad02f879e0708b24aac635a3333e89ccaf07f4c26f9330109b', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'reset-password/index.html': {size: 225, hash: 'f0d9e0108796e2e7b54a5ebcd679ace5ab8824b2f86a04284da20f61efcdadf1', text: () => import('./assets-chunks/reset-password_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 22884, hash: '9e220ec28be01ee103e418ad9fe14e1e8444a580e95d848e94b1d955ad8ea38f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'select-plan/index.html': {size: 16247, hash: '3318a62d72f25fad02f879e0708b24aac635a3333e89ccaf07f4c26f9330109b', text: () => import('./assets-chunks/select-plan_index_html.mjs').then(m => m.default)},
-    'signup/index.html': {size: 16247, hash: '3318a62d72f25fad02f879e0708b24aac635a3333e89ccaf07f4c26f9330109b', text: () => import('./assets-chunks/signup_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7730, hash: 'a84eaf7600ef9730fa111fb67724051ea0225d321898f38fb46401dde5777607', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7332, hash: '536c79ff4a8ab4e2b7ad241544ede65a16b9e58d2fd5ab15786bdc121bac2136', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 22891, hash: 'a7abad34adb467de63c60ce4507fa563e59cd218fd82dc95318770942623cf4b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'reset-password/index.html': {size: 246, hash: '67f4f62f46030e5d98599ee0d3c0acf28d4ee01bc7149bcefcdbf2aa748ff131', text: () => import('./assets-chunks/reset-password_index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 16254, hash: '0491dddc1498bd4e213e9cb2ec80ff4c442c7e23a34b7a614c943875f57c6dc7', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'signup/index.html': {size: 16254, hash: '0491dddc1498bd4e213e9cb2ec80ff4c442c7e23a34b7a614c943875f57c6dc7', text: () => import('./assets-chunks/signup_index_html.mjs').then(m => m.default)},
+    'select-plan/index.html': {size: 16254, hash: '0491dddc1498bd4e213e9cb2ec80ff4c442c7e23a34b7a614c943875f57c6dc7', text: () => import('./assets-chunks/select-plan_index_html.mjs').then(m => m.default)},
     'styles-M3S3TEZ4.css': {size: 1298, hash: 'p7lU365+TkY', text: () => import('./assets-chunks/styles-M3S3TEZ4_css.mjs').then(m => m.default)}
   },
 };
